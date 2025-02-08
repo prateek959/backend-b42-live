68ec11b1d9aa45c1c0d48b93de5dcb41ab7a5abf
prateek959 <prateekkush8081@gmail.com>
2025-02-08 21:55:33 +0530

    add

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import db from './config/db.js';
+import cors from 'cors';
 import userRouter from './routes/user.route.js';
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/user',userRouter);
